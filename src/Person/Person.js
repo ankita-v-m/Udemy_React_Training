27fe7ab9080c09f0e6e120f9a00e12b121bded5d
AnkitaMarathe <ankitavmarathe2017@gmail.com>
2020-11-17 19:16:32 +0530

    3rd commit

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import './Person.css'
+
 const person = (props) => {
 //    return <p>I am a Person and I am {Math.floor(Math.random() * 50)} years  old!</p>
 
 return (
-    <div>
-        <p>I am {props.name} and I am {props.age} yrs old {props.children}.</p>
-        <p>I am git demo</p>
-        <p>I am git demo2</p>
+    <div className="Person">
+        <p onClick={props.click}>I am {props.name} and I am {props.age} yrs old {props.children}.</p>
+        <input type="text" onChange={props.changed}/>
     </div>
 
     )
